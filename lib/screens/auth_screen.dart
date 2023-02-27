082b53a75288202b040a71b4b8dccb1da75ab44f
boris <nikiforov5000@gmail.com>
2023-02-27 13:23:38 +0300

    change auth-screen layout

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -25,162 +25,165 @@ class AuthScreen extends StatelessWidget {
       body: Container(
         color: kDarkGreyColor,
         child: SafeArea(
-          child: Column(
-            children: [
-              TextField(
-                // controller: controllerLogin,
-                onChanged: (value) {
-                  email = value;
-                },
-                textAlign: TextAlign.center,
-                autofocus: true,
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.white,
-                ),
-                decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(
-                        width: 1,
-                        color: kLightGreyColor,
-                      )),
-                  hintText: 'Enter Email',
-                  hintStyle: TextStyle(
-                      color: kLightGreyColor, fontWeight: FontWeight.w100),
-                ),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              TextField(
-                // controller: controllerLogin,
-                obscureText: true,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      color: kSemiLightGreyColor,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Column(
+                    children: [
+                      TextField(
+                        // controller: controllerLogin,
+                        onChanged: (value) {
+                          email = value;
+                        },
+                        textAlign: TextAlign.center,
+                        autofocus: true,
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                        ),
+                        decoration: InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
+                                width: 1,
+                                color: kLightGreyColor,
+                              )),
+                          hintText: 'Enter Email',
+                          hintStyle: TextStyle(
+                              color: kLightGreyColor,
+                              fontWeight: FontWeight.w100),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      TextField(
+                        // controller: controllerLogin,
+                        obscureText: true,
 
-                onChanged: (value) {
-                  password = value;
-                },
-                textAlign: TextAlign.center,
-                autofocus: true,
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.white,
+                        onChanged: (value) {
+                          password = value;
+                        },
+                        textAlign: TextAlign.center,
+                        autofocus: true,
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                        ),
+                        decoration: InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
+                                width: 1,
+                                color: kLightGreyColor,
+                              )),
+                          hintText: 'Enter Password',
+                          hintStyle: TextStyle(
+                              color: kLightGreyColor,
+                              fontWeight: FontWeight.w100),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Row(
+                        children: [
+                          AddTaskScreenButton(
+                            label: 'Login',
+                            color: Colors.green,
+                            onTap: () async {
+                              print(email + ' ' + password);
+                              try {
+                                final user =
+                                    await _auth.signInWithEmailAndPassword(
+                                  email: email,
+                                  password: password,
+                                );
+                                if (user != null) {
+                                  print('user != null');
+                                  Navigator.pushNamed(context, TasksScreen.id);
+                                }
+                              } catch (e) {
+                                print(e);
+                              }
+                            },
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          AddTaskScreenButton(
+                            label: 'Register',
+                            color: Colors.blue,
+                            onTap: () async {
+                              print(email + ' ' + password);
+                              try {
+                                final newUser =
+                                    await _auth.createUserWithEmailAndPassword(
+                                  email: email,
+                                  password: password,
+                                );
+                                if (newUser != null) {
+                                  Navigator.pushNamed(context, TasksScreen.id);
+                                }
+                              } catch (e) {
+                                print(e);
+                              }
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-                decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(
-                        width: 1,
-                        color: kLightGreyColor,
-                      )),
-                  hintText: 'Enter Password',
-                  hintStyle: TextStyle(
-                      color: kLightGreyColor, fontWeight: FontWeight.w100),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Or',
+                    style: kLoginHeaderTextStyle.copyWith(fontSize: 30),
+                  ),
                 ),
-              ),
-              Row(
-                children: [
-                  AddTaskScreenButton(
-                    label: 'Login',
-                    color: Colors.green,
-                    onTap: () async {
-                      print(email + ' ' + password);
-                      try {
-                        final user = await _auth.signInWithEmailAndPassword(
-                          email: email,
-                          password: password,
+                AddTaskScreenButton(
+                  label: 'Sign-in with Google',
+                  color: Colors.blue,
+                  onTap: () async {
+                    print('Register with Google');
+                    try {
+                      final googleUser = await _googleSignIn.signIn();
+                      if (googleUser != null) {
+                        // Google Sign-In successful, now create user account in Firebase
+                        final googleAuth = await googleUser.authentication;
+                        final credential = GoogleAuthProvider.credential(
+                          accessToken: googleAuth.accessToken,
+                          idToken: googleAuth.idToken,
                         );
-                        if (user != null) {
-                          print('user != null');
-                          Navigator.pushNamed(context, TasksScreen.id);
-                        }
-                      } catch (e) {
-                        print(e);
-                      }
-                    },
-                  ),
-                  SizedBox(
-                    width: 5,
-                  ),
-                  AddTaskScreenButton(
-                    label: 'Register',
-                    color: Colors.blue,
-                    onTap: () async {
-                      print(email + ' ' + password);
-                      try {
-                        final newUser =
-                            await _auth.createUserWithEmailAndPassword(
-                          email: email,
-                          password: password,
-                        );
-                        if (newUser != null) {
-                          Navigator.pushNamed(context, TasksScreen.id);
-                        }
-                      } catch (e) {
-                        print(e);
-                      }
-                    },
-                  ),
-                ],
-              ),
-              AddTaskScreenButton(
-                label: 'Sign in with Google',
-                color: Colors.teal,
-                onTap: () async {
-                  try {
-                    final googleUser = await _googleSignIn.signIn();
-                    if (googleUser != null) {
-                      final googleAuth = await googleUser.authentication;
-                      final credential = GoogleAuthProvider.credential(
-                        accessToken: googleAuth.accessToken,
-                        idToken: googleAuth.idToken,
-                      );
-                      await _auth.signInWithCredential(credential);
-                      Navigator.pushNamed(context, TasksScreen.id);
-                    }
-                  } catch (e) {
-                    print('catch:');
-                    print(e);
-                  }
-                }
-                ,
-              ),
-              AddTaskScreenButton(
-                label: 'Register with Google',
-                color: Colors.blue,
-                onTap: () async {
-                  print('Register with Google');
-                  try {
-                    final googleUser = await _googleSignIn.signIn();
-                    print('user selected');
-                    print(googleUser);
-                    if (googleUser != null) {
-                      // Google Sign-In successful, now create user account in Firebase
-                      final googleAuth = await googleUser.authentication;
-                      final credential = GoogleAuthProvider.credential(
-                        accessToken: googleAuth.accessToken,
-                        idToken: googleAuth.idToken,
-                      );
-                      final userCredential =
-                      await _auth.signInWithCredential(credential);
+                        final userCredential =
+                            await _auth.signInWithCredential(credential);
 
-                      // Create user account in Firebase if it doesn't already exist
-                      if (userCredential.additionalUserInfo!.isNewUser) {
-                        final currentUser = _auth.currentUser;
-                        if (currentUser != null) {
-                          await currentUser.updateProfile(displayName: googleUser.displayName);
+                        // Create user account in Firebase if it doesn't already exist
+                        if (userCredential.additionalUserInfo!.isNewUser) {
+                          final currentUser = _auth.currentUser;
+                          if (currentUser != null) {
+                            await currentUser
+                                .updateDisplayName(googleUser.displayName);
+                          }
                         }
-                      }
 
-                      Navigator.pushNamed(context, TasksScreen.id);
+                        Navigator.pushNamed(context, TasksScreen.id);
+                      }
+                    } catch (e) {
+                      print('catch');
+                      print(e);
                     }
-                  } catch (e) {
-                    print('catch');
-                    print(e);
-                  }
-                },
-              ),
-            ],
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
